b90e8e22361c5e2c079cb622a2b29cbe7504a214
Guillermo Moreno <jmemox@gmail.com>
2025-10-17 08:07:28 -0600

    feat: manage handler errors with ResponseHandlerErrors concern

---
+++ app/controllers/concerns/v1/response_handler_errors.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module V1
+  module ResponseHandlerErrors
+    extend ActiveSupport::Concern
+
+    included do
+      rescue_from StandardError do |exception|
+        case exception
+        when ActiveRecord::RecordNotFound
+          handle_not_found(exception)
+        when ActiveRecord::RecordInvalid
+          handle_record_invalid(exception)
+        else
+          handle_internal_server_error(exception)
+        end
+      end
+    end
+
+    private
+
+    def handle_not_found(exception)
+      head :not_found
+    end
+
+    def handle_record_invalid(exception)
+      response_errors(exception.record, status: :unprocessable_entity)
+    end
+
+    def handle_internal_server_error_response(exception)
+      head :internal_server_error if Rails.env.production?
+
+      raise exception
+    end
+
+    def response_errors(object, status:)
+      render "shared/errors", locals: { object: }, status:
+    end
+  end
+end
